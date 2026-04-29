ab94156e8688982ffb76828685e5ee56dc31af7a
Mayank Gautam <138778742+ExcitingTuber@users.noreply.github.com>
2026-04-29 12:32:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,11 +72,9 @@ def home():
     if request.method == "POST":
         file = request.files['image']
         file_bytes = np.frombuffer(file.read(), np.uint8)
+
         img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-        
-        h, w = img.shape[:2]
-        scale = 224 / max(h, w)
-        img = cv2.resize(img, (int(w * scale), int(h * scale)))
+        img = cv2.resize(img, (128, 128))
 
         output = colorize_image(img)
 
